7d23008686cc6215fac06fe8f6c4c17283aa0f38
Mauro Del Rio <mauro@crs4.it>
2019-06-13 17:05:45 +0200

    adds timeseries test

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,6 +1,8 @@
+import datetime
 import unittest
+import uuid
 
-from tdm_ingestion.ingestion import Ingester
+from tdm_ingestion.ingestion import Ingester, TimeSeries, ValueMeasure
 from tests.dummies import DummyConsumer, DummyStorage, DummyConverter
 
 
@@ -14,7 +16,16 @@ class TestIngester(unittest.TestCase):
 
 
 class TestTimeSeries(unittest.TestCase):
-    pass
+    def test_to_dict(self):
+        now = datetime.datetime.utcnow()
+        sensorcode = uuid.uuid4()
+        value = 100
+        ts = TimeSeries(now, sensorcode, ValueMeasure(100))
+        time_format = '%Y-%m-%dT%H:%M:%SZ'
+        to_dict = ts.to_dict(time_format)
+        self.assertEqual(to_dict['time'], now.strftime(time_format))
+        self.assertEqual(to_dict['sensorcode'], str(sensorcode))
+        self.assertEqual(to_dict['measure'], {'value': value})
 
 
 if __name__ == '__main__':
