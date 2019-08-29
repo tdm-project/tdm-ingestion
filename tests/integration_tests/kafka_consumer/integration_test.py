6052fb3c49953cfc5cc1dda76f33405a1351eb40
Mauro Del Rio <mauro@crs4.it>
2019-08-29 11:48:16 +0200

    add integration tests utils

--- tests/integration_tests/kafka_consumer/integration_test.py
+++ tests/integration_tests/kafka_consumer/integration_test.py
@@ -6,43 +6,10 @@ import time
 import requests
 from kafka import KafkaProducer
 from tdm_ingestion.converters.ngsi_converter import NgsiConverter
+from tests.integration_tests.utils import check_docker_logs, docker_compose_up, \
+    try_func, docker_compose_down
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-def docker_compose_up():
-    subprocess.check_call(['docker-compose', 'up', '-d'])
-
-
-def docker_compose_down():
-    subprocess.check_call(['docker-compose', 'down'])
-
-
-def try_func(func, sleep, retry, *args, **kwargs):
-    """
-   func must return True or a similiar value
-    """
-    passed = False
-    counter = 0
-    res = None
-    while not passed or counter < retry:
-        try:
-            res = func(*args, **kwargs)
-            if res:
-                passed = True
-                break
-        except Exception as ex:
-            logging.exception(ex)
-        counter += 1
-        time.sleep(sleep)
-    if not passed:
-        raise RuntimeError('func % failed with args %s kwargs %s', func, args,
-                           kwargs)
-    return res
-
-
-def check_docker_logs(service):
-    subprocess.check_output(["docker-compose", "logs", service])
 
 
 def check_timeseries(base_url, sensor_id, params):
---
+++ tests/integration_tests/utils.py
@@ -0,0 +1,38 @@
+import logging
+import subprocess
+import time
+
+
+def docker_compose_up():
+    subprocess.check_call(['docker-compose', 'up', '-d'])
+
+
+def docker_compose_down():
+    subprocess.check_call(['docker-compose', 'down'])
+
+
+def try_func(func, sleep, retry, *args, **kwargs):
+    """
+   func must return True or a similiar value
+    """
+    passed = False
+    counter = 0
+    res = None
+    while not passed or counter < retry:
+        try:
+            res = func(*args, **kwargs)
+            if res:
+                passed = True
+                break
+        except Exception as ex:
+            logging.exception(ex)
+        counter += 1
+        time.sleep(sleep)
+    if not passed:
+        raise RuntimeError('func % failed with args %s kwargs %s', func, args,
+                           kwargs)
+    return res
+
+
+def check_docker_logs(service):
+    subprocess.check_output(["docker-compose", "logs", service])
