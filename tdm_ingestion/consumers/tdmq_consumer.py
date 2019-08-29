e93fd10f68236bee9a977063dcd9ff67ef64230b
Mauro Del Rio <mauro@crs4.it>
2019-08-29 10:03:42 +0200

    fixes create_from_json
    Signed-off-by: Mauro Del Rio <mauro@crs4.it>

--- tdm_ingestion/consumers/tdmq_consumer.py
+++ tdm_ingestion/consumers/tdmq_consumer.py
@@ -1,9 +1,10 @@
 from enum import Enum
-from typing import List, Any
+from typing import List, Any, Dict
 
 from tdm_ingestion.ingestion import Consumer
 from tdm_ingestion.models import EntityType
 from tdm_ingestion.tdmq import Client
+from tdm_ingestion.utils import import_class
 
 
 class BucketOperation(Enum):
@@ -13,6 +14,16 @@ class BucketOperation(Enum):
 
 
 class TDMQConsumer(Consumer):
+    @classmethod
+    def create_from_json(cls, json: Dict):
+        json = json or {}
+        client = json['client']
+        return cls(
+            import_class(client['class']).create_from_json(client['args']),
+            EntityType(**json['entity_type']['args']),
+            json['bucket'],
+            json['operation']
+        )
 
     def __init__(self,
                  client: Client,
