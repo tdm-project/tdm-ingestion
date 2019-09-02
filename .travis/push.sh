9d9e89ff0dc10a4486cd777f55e153fc8aad2c44
Mauro Del Rio <mauro@crs4.it>
2019-09-02 17:07:58 +0200

    fixes travis push

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,6 +1,6 @@
 set -x
 VERSION=$(cat VERSION)
 echo "${CI_PASS}" | docker login -u "${CI_USER}" --password-stdin
-TAG="$CONF-$VERSION"
+TAG="$BACKEND-$VERSION"
 docker tag "$IMG" "$IMG:$TAG"
 docker push "$IMG:$TAG"
