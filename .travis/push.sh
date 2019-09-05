aa28d23053f35a35f5bd2242e7f80cfabfc87cc1
Mauro Del Rio <mauro@crs4.it>
2019-09-05 18:43:24 +0200

    remove sync prefix in docker tag
    Signed-off-by: Mauro Del Rio <mauro@crs4.it>

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
 set -x
 VERSION=$(cat VERSION)
 echo "${CI_PASS}" | docker login -u "${CI_USER}" --password-stdin
-TAG="$BACKEND-$VERSION"
+TAG="$VERSION"
+if [[ $BACKEND == "async" ]]
+then
+    TAG="$BACKEND-$TAG"
+fi
 docker tag "$IMG" "$IMG:$TAG"
 docker push "$IMG:$TAG"
+
