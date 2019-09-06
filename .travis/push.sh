99397209c0b49e394407fced488f0b3835e59a55
Mauro Del Rio <mauro@crs4.it>
2019-09-06 14:50:48 +0200

    removes backend
    Signed-off-by: Mauro Del Rio <mauro@crs4.it>

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,10 +3,7 @@ set -x
 VERSION=$(cat VERSION)
 echo "${CI_PASS}" | docker login -u "${CI_USER}" --password-stdin
 TAG="$VERSION"
-if [[ $BACKEND == "async" ]]
-then
-    TAG="$BACKEND-$TAG"
-fi
+
 docker tag "$IMG" "$IMG:$TAG"
 docker push "$IMG:$TAG"
 
